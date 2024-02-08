a9f36fe46d201ace576448e56539b0e70e949a60
Maiara Ferreira <103464615+MaiaFerreira04@users.noreply.github.com>
2024-02-08 15:47:25 -0300

    refactor: ajustando o card

--- lib/widgets/cards/card_exibicao.dart
+++ lib/widgets/cards/card_exibicao.dart
@@ -26,40 +26,36 @@ class CardExibicao extends StatelessWidget {
       elevation: 3,
       margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-      child: Stack(
-        children: [
-          Container(
-            margin: const EdgeInsets.all(20),
-            child: Row(
+      child: Container(
+        margin: const EdgeInsets.all(20),
+        child: Row(
+          children: [
+            Stack(
               children: [
-                Stack(
-                  children: [
-                    CircleAvatar(
-                      radius: 50,
-                      backgroundColor: const Color.fromRGBO(90, 90, 90, 1),
-                      child: CircleAvatar(
-                        backgroundColor: const Color.fromRGBO(244, 67, 54, 1),
-                        radius: 49,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(100),
-                          child: Image.asset(AssetsPath.USER, fit: BoxFit.fill, width: double.infinity),
-                        ),
-                      ),
+                CircleAvatar(
+                  radius: 50,
+                  backgroundColor: const Color.fromRGBO(90, 90, 90, 1),
+                  child: CircleAvatar(
+                    backgroundColor: const Color.fromRGBO(244, 67, 54, 1),
+                    radius: 49,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: Image.asset(AssetsPath.USER, fit: BoxFit.fill, width: double.infinity),
                     ),
-                  ],
-                ),
-                const SizedBox(width: 20),
-                Flexible(
-                  child: CsInformacoes(
-                    nome: nome,
-                    numero: numero,
-                    email: email,
                   ),
                 ),
               ],
             ),
-          ),
-        ],
+            const SizedBox(width: 20),
+            Flexible(
+              child: CsInformacoes(
+                nome: nome,
+                numero: numero,
+                email: email,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
